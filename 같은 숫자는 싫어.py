456f65086f6b3a8bf6a2e9ccd8fd79f83699f5d7
MingyuKIM615 <86060194+MingyuKIM615@users.noreply.github.com>
2022-05-24 12:25:20 +0900

    Create 같은 숫자는 싫어.py

---
+++ 같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+    
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        elif arr[i-1] != arr[i]:
+            answer.append(arr[i])
+    
+    return answer
